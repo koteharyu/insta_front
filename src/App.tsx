2b01b450f7c488eeaf42e7f4547fae072393b914
haryu <hryau6@gmail.com>
2021-10-15 11:59:59 +0900

    add: react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import {BrowserRouter} from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
     <ChakraProvider>
+      <BrowserRouter>
       
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
