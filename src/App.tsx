41ef03203e5b2c4ab338299a09aa7c9ff89e37d3
haryu <hryau6@gmail.com>
2021-10-15 12:22:24 +0900

    add: header

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { ChakraProvider } from '@chakra-ui/react'
 import {BrowserRouter} from 'react-router-dom'
 import { Router } from './router/Router';
 import './App.css';
+import { Header } from './components/templates/Header';
 
 function App() {
   return (
     <ChakraProvider>
       <BrowserRouter>
+        <Header />
         <Router />
       </BrowserRouter>
     </ChakraProvider>
---
+++ src/components/templates/Header.tsx
@@ -0,0 +1,45 @@
+import { memo, VFC } from "react";
+import { Flex, Heading, Box, Link, useDisclosure } from '@chakra-ui/react'
+import { useHistory } from 'react-router-dom'
+import { MenuIconButton } from "../atoms/button/MenuIconButton";
+import { MenuDrawer } from "../molecules/MenuDrawer";
+
+export const Header: VFC = memo(() => {
+
+  const history = useHistory()
+
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  const onClickTimeLine = () => history.push('/')
+  const onClickLogin = () => history.push('/login')
+  const onClickSignUp = () => history.push('/signup')
+
+  return (
+    <>
+      <Flex as="nav" bg="white" color="gray.505" align="center" justify="space-between" padding={{ base: 3, md: 5 }}>
+        <Flex align="center" as="a" mr={8} _hover={{ cursor: "pointer" }} onClick={onClickTimeLine}>
+          <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>Sonu Sonu</Heading>
+        </Flex>
+        <Flex align="center" fontSize="sm" flexGrow={2} display={{ base: "none", md: "flex" }}>
+          <Box pr={4}>
+            <Link onClick={onClickTimeLine}>
+              Home
+            </Link>
+          </Box>
+          <Box pr={4}>
+            <Link onClick={onClickLogin}>
+              Menu
+            </Link>
+          </Box>
+          <Box pr={4}>
+            <Link onClick={onClickSignUp}>
+              Gournal
+            </Link>
+          </Box>
+        </Flex>
+        <MenuIconButton onOpen={onOpen} />
+      </Flex>
+      <MenuDrawer onClose={onClose} isOpen={isOpen} onClickTimeLine={onClickTimeLine} onClickLogin={onClickLogin} onClickSignUp={onClickSignUp} />
+    </>
+  )
+})
